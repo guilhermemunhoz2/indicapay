6914902c21fc481db49b026601078fe36fac6465
guilhermemunhoz2 <GUILHERME-MUNHOZ@OUTLOOK.COM>
2025-04-22 23:26:35 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
 // Importação do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
-// Configurações do Firebase
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyA100UaJzXM2G5m7I5BvS1hnhPcJseNlCI",
+  apiKey: "AIzaSyA10U0aJzXM2G5m7I5BvS1hnhPcJseNlCI",
   authDomain: "indicapay-6162f.firebaseapp.com",
   projectId: "indicapay-6162f",
   storageBucket: "indicapay-6162f.appspot.com",
   messagingSenderId: "351397382935",
-  appId: "1:351397382935:web:3027a03a266a5abee0552",
+  appId: "1:351397382935:web:3027a03a266a5abee00552",
   measurementId: "G-KRGNYNVEDT"
 };
 
@@ -17,5 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Exporta auth e métodos para usar no cadastro e login
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+// Exporta instância de autenticação e métodos para uso no cadastro e login
+export { auth, createUserWithEmailAndPassword };
